ae009cdffa506bf6d5e8cd957a8a6c963d853dfe
Harrison B. Prosper <hbprosper@gmail.com>
2018-03-31 23:56:26 -0400

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$DIR/bin:$PATH
-export PYTHIA8=$HOME/external
+export PYTHIA8=$DIR/..
 export PYTHIA8DATA=`pythia8-config`
 export LD_LIBRARY_PATH=$PYTHIA8/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$DIR/python:$PYTHONPATH
