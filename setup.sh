228e70fd1dfb8f56defb9efd143ab0995235c4f4
Harrison B. Prosper <hbprosper@gmail.com>
2018-04-01 16:09:48 -0400

    add DYLD_LIBRARY_PATH and LHAPDF_DATA_PATH to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$DIR/bin:$PATH
-export PYTHIA8=$DIR/..
+export PYTHIA8=$EXTERNAL
 export PYTHIA8DATA=`pythia8-config`
 export LD_LIBRARY_PATH=$PYTHIA8/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$PYTHIA8/lib:$DYLD_LIBRARY_PATH
+export LHAPDF_DATA_PATH=$EXTERNAL/share/LHAPDF/PDFsets
 export PYTHONPATH=$DIR/python:$PYTHONPATH
